af30cbbc6b54fb935ee921bc366a5c8927477d4c
MahaultGalibert <mahault.destaintot@gmail.com>
2024-05-27 22:38:02 +0200

    rookie-add-rating-to-movie-preview

--- src/components/MoviePreview/MoviePreview.tsx
+++ src/components/MoviePreview/MoviePreview.tsx
@@ -3,6 +3,7 @@ import { Movie } from "../../models";
 import styles from "./MoviePreview.module.css";
 
 const MoviePreview = ({ movie }: { movie: Movie }) => {
+  const rating = Math.floor((movie.vote_average * 5) / 10);
   const goToMovieDetails = () => {
     // A DEFINIR EXPERT
   };
@@ -15,7 +16,21 @@ const MoviePreview = ({ movie }: { movie: Movie }) => {
           backgroundImage: `url(${POSTER_URL_PREFIX + movie.poster_path})`,
         }}
       ></div>
-      <div className={styles.movieTitle}>{movie.title}</div>
+      <div className={styles.movieInfo}>
+        <div className={styles.movieTitle}>{movie.title}</div>
+        <div className={styles.starRating}>
+          {[...Array(5)].map((_, index) => (
+            <span
+              key={index}
+              className={`${styles.star} ${
+                index < rating ? styles.orange : ""
+              }`}
+            >
+              ★
+            </span>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
