b4befb12e3c7972a73e0300d9dcbb125fdd13b71
MahaultGalibert <mahault.destaintot@gmail.com>
2024-05-27 22:31:04 +0200

    rookie-display-basic-movie-preview

--- src/components/MoviePreview/MoviePreview.tsx
+++ src/components/MoviePreview/MoviePreview.tsx
@@ -1,11 +1,23 @@
+import { POSTER_URL_PREFIX } from "../../App";
+import { Movie } from "../../models";
 import styles from "./MoviePreview.module.css";
 
-const MoviePreview = () => {
+const MoviePreview = ({ movie }: { movie: Movie }) => {
   const goToMovieDetails = () => {
     // A DEFINIR EXPERT
   };
 
-  return <div className={styles.movieCard} onClick={goToMovieDetails}></div>;
+  return (
+    <div className={styles.movieCard} onClick={goToMovieDetails}>
+      <div
+        className={styles.moviePoster}
+        style={{
+          backgroundImage: `url(${POSTER_URL_PREFIX + movie.poster_path})`,
+        }}
+      ></div>
+      <div className={styles.movieTitle}>{movie.title}</div>
+    </div>
+  );
 };
 
 export default MoviePreview;
